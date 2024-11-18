d78ddb2270f0b6fd7e897ec72d758caecbde30a4
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2024-11-18 22:12:38 +0100

    quest4

--- quest4/part3.rb
+++ quest4/part3.rb
@@ -8,41 +8,22 @@ class App
   end
   
   state do
-    table :nails, [:pos, :iter] => [:height]
-    table :pairings, [:pos, :iter, :pair_id] => [:height]
-    table :switches, [:pos, :iter] => [:height]
-    table :disordered, [:iter]
-    table :unsorted, [:pos, :iter] => [:height]
-  end
-
-  bloom :do_sort do
-    pairings <= nails do |n|
-      base = (n.pos - n.iter % 2)
-      [n.pos, n.iter, base - base % 2, n.height]
-    end
-    switches <= (pairings * pairings).pairs(:pair_id => :pair_id, :iter => :iter).flat_map do |p1, p2|
-      if p1.pos < p2.pos && p1.height > p2.height
-        [[p1.pos, p1.iter, p2.height], [p2.pos, p2.iter, p1.height]]
-      end
-    end
-    disordered <= switches {|s| [s.iter]}
-    unsorted <= (disordered * nails).matches {|d, n| n}
-    nails <= (switches * unsorted).outer(:pos => :pos, :iter => :iter) do |s,u|
-      if s.pos
-        [s.pos, s.iter + 1, s.height]
-      else
-        [u.pos, u.iter + 1, u.height]
-      end
-    end
+    table :nails, [:pos] => [:height]
+    table :median, [:height]
+    lmin :level
   end
 
   bloom :hammer do
-    stdio <~ nails.inspected
+    level <= median {|m| m.height}
+    temp :strikes <= nails {|n| [n.pos, (n.height - level.reveal).abs]}
+    stdio <~ strikes.group([], sum(:height)).inspected
   end
 end
 
 app = App.new(:stdin => $stdin)
-app.nails <+ ARGF.each_with_index.map do |l, j|
-  [j, 0, l.chomp.to_i]
+values = ARGF.map {|l| l.chomp.to_i}.sort
+app.nails <+ values.each_with_index.map do |v, j|
+  [j, v]
 end
+app.median <+ [[values[values.length / 2]]]
 app.tick
